1d53a586db308c481a3b504570dadd37d51c2167
Jorge Mauricio <jorge.mauricio@me.com>
2018-08-10 12:44:42 -0500

    agregar archivo de algoritmo.py

---
+++ algoritmo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+#######################################
+# Script que procesa la información del WRF
+# para la parametrización de los datos
+# Author: Jorge Mauricio
+# Email: jorge.ernesto.mauricio@gmail.com
+# Date: Created on Thu Sep 28 08:38:15 2017
+# Version: 1.0
+#######################################
+"""
+
+# librerías
+import os
+import pandas as pd
+import numpy as np
+import time
+from time import gmtime, strftime
